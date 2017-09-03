629efbe02bf3517920d365bdbefbd2eb4f48cf3c
aeris <patrickyamut@yahoo.com>
2017-09-04 05:29:34 +0800

    Fix ` ;` semicolon

--- app.js
+++ app.js
@@ -48,21 +48,10 @@ if (message.content.startsWith(prefix + 'embed')) {
 	let noto = message.content.split(" ").slice(1).join(" ");
 	message.delete();
 	var embed = new Discord.RichEmbed();
-
   embed.setColor("#53A6F3")
-
   .setDescription(noto)
-
-  message.channel.sendEmbed(
-
-    embed, {
-
-      disableEveryon: true
-
-    }
-
-  );
-}
+  message.channel.sendEmbed(embed, {disableEveryon: true});
+};
 
 });
 
