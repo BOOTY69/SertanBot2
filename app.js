362b3ca641b1562307475c9988f870f15ff4c81d
aeris <patrickyamut@yahoo.com>
2017-09-05 01:11:48 +0800

    Added prune/purge command

--- app.js
+++ app.js
@@ -38,7 +38,23 @@ client.on('message', message => {
 	if (message.content.startsWith(prefix + 'Robots')) {
 		message.channel.send('You can buy War Robots Premuim Soon');
 	};
-
+	if (message.content.startsWith(prefix + 'prune')) {
+if (!message.member) return;  
+    try {
+    let amount = message.content.split(" ").splice(1, 2).join(" ");
+        if(amount < 1) {
+            return message.reply("The amount of messages to remove is 1-100.");
+        }
+        if(amount >100) {
+            return message.reply("Choose a number between 1-100.");
+        }
+        message.channel.bulkDelete(amount);
+        message.delete()
+      message.channel.sendMessage("Purged: " + amount + " Messages").then(m => m.delete(2500));
+    } catch(err) {
+			message.channel.sendCode('js', `${err}\nThe role Bot Commander was not found.`)
+		};
+	}
 
 if (message.content.startsWith(prefix + 'help')) {
 	message.channel.send("Commands `CFeatures, CopsBuy, AS , credits, ping `")
